079332b3c45cf6862414d9fff4175d01ba730ea7
Jisie DM <jisie.david@gmail.com>
2020-12-06 23:10:30 -0800

    Adding intial repo setup instructions

---
+++ 04-setting-up-your-repo.md
@@ -0,0 +1,61 @@
+# Setting up your capstone application
+
+Please read through all the instructions thoroughly before starting and reach out and ask questions if you are unsure about anything!
+
+### Initial Setup
+
+1. In the directory that you want to create your application, run `npx create-react-app the-name-of-your-app`.
+1. `cd` into the application directory that has been created.
+1. Add `.eslintcache` to the bottom of your `.gitignore`.
+1. Open your `README.md` and delete all text in the file.
+1. Add and commit your changes on `main`.
+1. Go to your empty capstone repository on Github and follow the instructions under the heading **…or push an existing repository from the command line** (The second set of instructions!)
+1. Refresh your repo in your browser to make sure your repo now contains your application.
+1. In your terminal, make sure to checkout a branch before you start working.
+
+### Persistent storage with JSON-SERVER
+
+You can set up your json-server API to be a part of your application directory (like Glassdale) or a separate directory (like Kennel). If you decide to keep your API a separate directory, make sure to create a new repository for it and push it up.
+
+### Routing
+
+If you plan to use routing in your application, in your application root directory, run `npm i --save react-router-dom` to install the package.
+
+### Authentication
+
+**Before starting this, make sure you have added, commited and pushed up your current branch. Then checkout a new branch from the main branch.**
+
+Make sure you are in the root directory of your application and run the following command:
+
+```sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/nss-day-cohort-44/CAPSTONE-INFORMATION/scripts/auth.sh)"
+
+```
+
+Place this in your main app React component (i.e., App.js, Kennel.js, etc.)
+Make sure to add import statements for `Route` and `Redirect` from `react-router-dom`. And import the Login and Register components.
+
+```js
+        <Route render={() => {
+          // The user id is saved under the key app_user_id in local Storage. Change below if needed!
+            if (localStorage.getItem("app_user_id")) {
+                return (
+                    <>
+                        //Components that are rendered when the user is authenticated go inside this React fragment
+                    </>
+                )
+            } else {
+                return <Redirect to="/login" />
+            }
+        }} />
+
+        <Route path="/login" render={props => <Login {...props} />} />
+        <Route path="/register" render={props => <Register {...props} />} />
+    </>
+```
+
+### Github Workflow
+
+- Please make sure you are always working on a branch and commit very, very often.
+- Keep your commits and pull requests small and manageable.
+- Remember to push up on your branch any time you will step away from your machine. Remember, if it's on Github, we can always find a way to get it back in case of a sticky situation.
---
+++ scripts/auth.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+set -u
+
+mkdir -p ./src/components/auth && cd $_
+
+echo 'import React, { useRef } from "react"
+import "./Login.css"
+
+export const Register = (props) => {
+    const firstName = useRef()
+    const lastName = useRef()
+    const email = useRef()
+    const password = useRef()
+    const verifyPassword = useRef()
+    const passwordDialog = useRef()
+    const conflictDialog = useRef()
+
+    const existingUserCheck = () => {
+        // If your json-server URL is different, please change it below!
+        return fetch(`http://localhost:8088/users?email=${email.current.value}`)
+            .then(_ => _.json())
+            .then(user => !!user.length)
+    }
+
+    const handleRegister = (e) => {
+        e.preventDefault()
+
+        if (password.current.value === verifyPassword.current.value) {
+            existingUserCheck()
+                .then((userExists) => {
+                    if (!userExists) {
+                        // If your json-server URL is different, please change it below!
+                        fetch("http://localhost:8088/users", {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify({
+                                email: email.current.value,
+                                password: password.current.value,
+                                name: `${firstName.current.value} ${lastName.current.value}`
+                            })
+                        })
+                            .then(_ => _.json())
+                            .then(createdUser => {
+                                if (createdUser.hasOwnProperty("id")) {
+                                    // The user id is saved under the key app_user_id in local Storage. Change below if needed!
+                                    localStorage.setItem("app_user_id", createdUser.id)
+                                    props.history.push("/")
+                                }
+                            })
+                    }
+                    else {
+                        conflictDialog.current.showModal()
+                    }
+                })
+        } else {
+            passwordDialog.current.showModal()
+        }
+    }
+
+    return (
+        <main style={{ textAlign: "center" }}>
+
+            <dialog className="dialog dialog--password" ref={passwordDialog}>
+                <div>Passwords do not match</div>
+                <button className="button--close" onClick={e => passwordDialog.current.close()}>Close</button>
+            </dialog>
+
+            <dialog className="dialog dialog--password" ref={conflictDialog}>
+                <div>Account with that email address already exists</div>
+                <button className="button--close" onClick={e => conflictDialog.current.close()}>Close</button>
+            </dialog>
+
+            <form className="form--login" onSubmit={handleRegister}>
+                <h1 className="h3 mb-3 font-weight-normal">Please Register for Application Name</h1>
+                <fieldset>
+                    <label htmlFor="firstName"> First Name </label>
+                    <input ref={firstName} type="text" name="firstName" className="form-control" placeholder="First name" required autoFocus />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="lastName"> Last Name </label>
+                    <input ref={lastName} type="text" name="lastName" className="form-control" placeholder="Last name" required />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="inputEmail"> Email address </label>
+                    <input ref={email} type="email" name="email" className="form-control" placeholder="Email address" required />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="inputPassword"> Password </label>
+                    <input ref={password} type="password" name="password" className="form-control" placeholder="Password" required />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="verifyPassword"> Verify Password </label>
+                    <input ref={verifyPassword} type="password" name="verifyPassword" className="form-control" placeholder="Verify password" required />
+                </fieldset>
+                <fieldset>
+                    <button type="submit"> Sign in </button>
+                </fieldset>
+            </form>
+        </main>
+    )
+}
+' > ./Register.js
+
+echo 'import React, { useRef } from "react"
+import { Link } from "react-router-dom";
+import "./Login.css"
+
+
+export const Login = props => {
+    const email = useRef()
+    const password = useRef()
+    const existDialog = useRef()
+    const passwordDialog = useRef()
+
+    const existingUserCheck = () => {
+        // If your json-server URL is different, please change it below!
+        return fetch(`http://localhost:8088/users?email=${email.current.value}`)
+            .then(_ => _.json())
+            .then(user => user.length ? user[0] : false)
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+
+        existingUserCheck()
+            .then(exists => {
+                if (exists && exists.password === password.current.value) {
+                    // The user id is saved under the key app_user_id in local Storage. Change below if needed!
+                    localStorage.setItem("app_user_id", exists.id)
+                    props.history.push("/")
+                } else if (exists && exists.password !== password.current.value) {
+                    passwordDialog.current.showModal()
+                } else if (!exists) {
+                    existDialog.current.showModal()
+                }
+            })
+    }
+
+    return (
+        <main className="container--login">
+            <dialog className="dialog dialog--auth" ref={existDialog}>
+                <div>User does not exist</div>
+                <button className="button--close" onClick={e => existDialog.current.close()}>Close</button>
+            </dialog>
+            <dialog className="dialog dialog--password" ref={passwordDialog}>
+                <div>Password does not match</div>
+                <button className="button--close" onClick={e => passwordDialog.current.close()}>Close</button>
+            </dialog>
+            <section>
+                <form className="form--login" onSubmit={handleLogin}>
+                    <h1>Application Name</h1>
+                    <h2>Please sign in</h2>
+                    <fieldset>
+                        <label htmlFor="inputEmail"> Email address </label>
+                        <input ref={email} type="email"
+                            id="email"
+                            className="form-control"
+                            placeholder="Email address"
+                            required autoFocus />
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="inputPassword"> Password </label>
+                        <input ref={password} type="password"
+                            id="password"
+                            className="form-control"
+                            placeholder="Password"
+                            required />
+                    </fieldset>
+                    <fieldset>
+                        <button type="submit">
+                            Sign in
+                        </button>
+                    </fieldset>
+                </form>
+            </section>
+            <section className="link--register">
+                <Link to="/register">Not a member yet?</Link>
+            </section>
+        </main>
+    )
+}
+' > ./Login.js
+
+touch ./Login.css
